6083e070d4d81dd44dc03165bd570cffbf6e4c6f
Vadzim Matsiushonak <118316719+VadzimMatsiushonak@users.noreply.github.com>
2023-10-16 01:32:13 +0300

    PM-1 Add prisma database (#2)
    - Created User schema
    - Add create and read User operations

--- index.js
+++ index.js
@@ -1,53 +1,70 @@
 const express = require('express')
+const {body, validationResult} = require('express-validator');
 const app = express()
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
+
 const PORT = process.env.PORT || 3000
-
 app.use(express.json())
-
-let users = [
-    {
-        login: 'root',
-        password: 'pass'
-    },
-    {
-        login: 'vadzim',
-        password: 'pass'
-    }
-]
-
-app.get('/users', (req, res) => {
-    res.status(200).send(users)
-})
-
-app.get('/users/:id', (req, res) => {
-    const {id} = req.params
-
-    if (!id || id >= users.length) {
-        res.status(200).send({message: `No user found with id ${id}`})
-    }
-    res.status(200).send(users[id])
-})
-
-app.post('/users/:id', (req, res) => {
-    const {id} = req.params
-    const {login} = req.body
-    const {password} = req.body
-
-    if (!id || id >= users.length) {
-        res.status(200).send({message: `No user found with id ${id}`})
-    }
-
-    let user = users[id];
-    if (login) {
-        user.login = login
-    }
-    if (password) {
-        user.password = password
-    }
-    res.status(200).send(user)
-})
-
 app.listen(
     PORT,
     () => console.log(`App initialized on ${PORT}`)
 )
+
+app.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany()
+    res.status(200).send(users)
+})
+
+app.get('/users/:id', async (req, res) => {
+    const {id} = req.params
+
+    if (!id || isNaN(id)) {
+        res.status(400).send({message: `Unable to process the request with id ${id}`})
+    }
+
+    const user = await prisma.user.findUnique({
+        where: {
+            id: +id,
+        },
+    })
+
+    res.status(200).send(user)
+})
+
+app.post('/users', [
+    body('email').notEmpty().withMessage('Email is required.'),
+    body('password').notEmpty().withMessage('Password is required.')
+], async (req, res) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const user = await prisma.user.create({
+        data: req.body,
+    })
+
+    res.status(200).send(user)
+})
+
+
+async function main() {
+    // const users = await prisma.user.findMany()
+    // console.log(users)
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
+
+function isNumeric(num) {
+    return !isNaN(num)
+}
